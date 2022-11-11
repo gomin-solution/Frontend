08a28f21459027a6e46d5469e65aa6927a826c2b
Jeongpyo Hong <112886992+Jeongpyo-Hong@users.noreply.github.com>
2022-11-11 18:28:44 +0900

    [chore] 중복체크 기능 구현 완

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
@@ -11,6 +11,11 @@ import { Header1 } from "../components/header/Header";
 const Signup = () => {
   const nav = useNavigate();
 
+  // 아이디, 닉네임 중복체크
+  const [idDub, setIdDub] = useState(false);
+  const [nickDub, setNickDub] = useState(false);
+
+  // react-hook-form 사용
   const {
     register,
     handleSubmit,
@@ -21,9 +26,11 @@ const Signup = () => {
   // password 변수에 키 값 할당
   const password = watch("password");
 
+  // 회원가입 제출
   const onSubmit = async (data) => {
-    console.log("data", data);
-    console.log(typeof data.adult);
+    if (idDub === false || nickDub === false) {
+      return window.alert("아이디와 닉네임 모두 중복체크 해주세요.");
+    }
     try {
       const res = await instance.post("/signup", data);
       if (res.status === 200) {
@@ -43,24 +50,32 @@ const Signup = () => {
     }
   };
 
+  // 아이디 중복검사
   const idCheck = async () => {
     const userId = watch("userId");
+    console.log("userId", userId);
     const res = await instance.post("/signup/check", { userId: userId });
+    console.log("res1", res);
     if (res.status === 200) {
       alert("사용 가능한 아이디입니다.");
     } else {
       alert("이미 사용중인 아이디입니다.");
     }
+    setIdDub(!idDub);
   };
 
+  // 닉네임 중복검사
   const nickCheck = async () => {
     const nickname = watch("nickname");
+    console.log("nickname", nickname);
     const res = await instance.post("/signup/check", { nickname: nickname });
+    console.log("res2", res);
     if (res.status === 200) {
       alert("사용 가능한 닉네임입니다.");
     } else {
       alert("이미 사용중인 닉네임입니다.");
     }
+    setNickDub(!idDub);
   };
 
   return (
@@ -69,6 +84,7 @@ const Signup = () => {
       <StFormContainer as="form" onSubmit={handleSubmit(onSubmit)}>
         <StTitle>회원가입</StTitle>
         <StInputWrap>
+          {/* ----- 아이디 ----- */}
           <StInputInnerWrap>
             <StInput
               placeholder="아이디"
@@ -97,6 +113,7 @@ const Signup = () => {
           ) : (
             <StErr>{errors?.userId?.message}</StErr>
           )}
+          {/* ----- 닉네임 ----- */}
           <StInputInnerWrap>
             <StInput
               placeholder="닉네임"
@@ -121,6 +138,7 @@ const Signup = () => {
           ) : (
             <StErr>{errors?.nickname?.message}</StErr>
           )}
+          {/* ----- 패스워드 ----- */}
           <StInput
             type="password"
             placeholder="패스워드"
@@ -148,6 +166,7 @@ const Signup = () => {
           ) : (
             <StErr>{errors?.password?.message}</StErr>
           )}
+          {/* ----- 패스워드 확인 ----- */}
           <StInput
             type="password"
             placeholder="패스워드 확인"
@@ -159,12 +178,13 @@ const Signup = () => {
               },
             })}
           />
-          {errors?.password?.message === undefined ? (
+          {errors?.confirm?.message === undefined ? (
             <StCheck>패스워드와 동일하게 다시 작성해주세요.</StCheck>
           ) : (
-            <StErr>{errors?.password?.message}</StErr>
+            <StErr>{errors?.confirm?.message}</StErr>
           )}
         </StInputWrap>
+        {/* ----- 성인 여부 ----- */}
         <StAdult>
           <span
             style={{

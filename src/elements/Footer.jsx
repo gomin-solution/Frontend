a792a80908b09d5dbfa87b8ade40b22722f9fa99
doeun <drakequation@naver.com>
2022-11-13 01:05:27 +0900

    [feat] footer UI 작업

--- src/elements/Footer.jsx
+++ src/elements/Footer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import styled from "styled-components";
 
 function Footer() {
-  return <div>Footer</div>;
+  return <StBlock>Footer</StBlock>;
 }
 
 export default Footer;
+
+/*전체 블록 */
+const StBlock = styled.div`
+  width: 100%;
+  height: 4rem;
+
+  z-index: 10;
+  bottom: 0;
+
+  background-color: ${(props) => props.theme.boxColors.bg};
+`;

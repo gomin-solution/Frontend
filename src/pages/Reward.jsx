f72b9e70e42e2cbc083e68b1420c85cf29334de2
doeun <drakequation@naver.com>
2022-11-14 01:35:49 +0900

    [feat] 주요 페이지에 footer, header 설정

--- src/pages/Reward.jsx
+++ src/pages/Reward.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Header1 } from "../elements/Header";
+import styled from "styled-components";
+import Footer from "../elements/Footer";
 
 function Reward() {
-  return <div>Reward</div>;
+  return (
+    <>
+      <Header1 title={"획득한 종이"} />
+      <Stcontainer>획득한 종이</Stcontainer>
+      <Footer />
+    </>
+  );
 }
 
 export default Reward;
+
+const Stcontainer = styled.div`
+  width: 100%;
+  position: absolute;
+  overflow-y: scroll;
+`;

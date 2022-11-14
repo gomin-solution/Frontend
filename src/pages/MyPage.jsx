9bcac586e0908f01e9152f17c571527d4717b1cb
Jeongpyo Hong <112886992+Jeongpyo-Hong@users.noreply.github.com>
2022-11-14 11:47:07 +0900

    마이페이지 삭제

--- src/pages/MyPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyPage = () => {
-  return <div>MyPage</div>;
-};
-
-export default MyPage;
--- src/pages/Mypage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Mypage = () => {
-  return <div>마이페이지</div>;
-};
-
-export default Mypage;

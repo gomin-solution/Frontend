14bfe1dafbf727ec97e2258a9e12005736c82bcb
doeun <drakequation@naver.com>
2022-11-26 12:13:33 +0900

    [chore] 모달 알러트 부분 수정중

--- src/elements/Alert.jsx
+++ src/elements/Alert.jsx
@@ -1,3 +1,4 @@
+import Modal from "@mui/material/Modal";
 import Swal from "sweetalert2";
 
 /* 버튼 1개 and 확인 후 이동 X */
@@ -56,3 +57,50 @@ export const Alert4 = (text) => {
     width: "20rem",
   });
 };
+
+/*리워드 모달 */
+export function RewardModal({ modalOpen, closeModal, tip }) {
+  return (
+    <div>
+      <Modal
+        open={modalOpen}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        hideBackdrop={true}
+      >
+        <StBigPrev>
+          <ClearIcon className="clear" onClick={closeModal} />
+          <StTip>
+            {tip.map((item) => (
+              <div key={item}>{item}</div>
+            ))}
+          </StTip>
+        </StBigPrev>
+      </Modal>
+    </div>
+  );
+}
+
+/*팁 띄우기*/
+const StTip = styled.div`
+  width: 80%;
+  height: 30%;
+  background-color: #ffffff;
+
+  position: absolute;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  gap: 1rem;
+
+  div {
+    font-size: 1.5rem;
+  }
+`;
